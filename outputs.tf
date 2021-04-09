f915b74a0f736e6b007bf8d6493b30c963010bf5
Rick Bauer <rickbaue@cisco.com>
2021-04-09 14:01:09 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,15 +3,15 @@
 
 provider "dnacenter" {
   # Cisco DNA Center user name
-  # username = "admin"
+  username = "DNAC_USERNAME"
   # it can be set using the environment variable DNAC_USERNAME
 
   # Cisco DNA Center password
-  # password = "@$@#%%@#$"
+  password = "DNAC_PASSWORD"
   # it can be set using the environment variable DNAC_PASSWORD
 
   # Cisco DNA Center base URL, FQDN or IP
-  # base_url = "https://x.x.x.x"
+  base_url = "DNAC_BASE_URL"
   # it can be set using the environment variable DNAC_BASE_URL
 
 
@@ -22,15 +22,6 @@ provider "dnacenter" {
   # Boolean to enable or disable SSL certificate verification
   ssl_verify = "false"
   # it can be set using the environment variable DNAC_SSL_VERIFY
-}
-
-variable "user_information" {
-  type = object({
-    username = "DNAC_USERNAME"
-    password = "DNAC_PASSWORD"
-    base_url = "DNAC_BASE_URL"
-  })
-  sensitive = true
 }
 
 # Configure CLI credential
