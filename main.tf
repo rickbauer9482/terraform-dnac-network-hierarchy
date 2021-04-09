d1d8a52812d9a2d20739c8401adcf139cba0722a
Rick Bauer <rickbaue@cisco.com>
2021-04-09 11:13:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,5 +4,6 @@ terraform {
       source  = "cisco-en-programmability/dnacenter"
       version = "0.0.3"
     }
+    token = gq1FL04zDaO50w.atlasv1.lJfxHmMy7Dcxu409eTKw02uz77En7oFyEjGaIe6aPRqQiNJjSJWXDO3RgLaCzX6ct5A
   }
 }
